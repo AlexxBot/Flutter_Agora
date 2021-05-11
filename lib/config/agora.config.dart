10bd78766826a3b5d7f7ff45385a351765b49b25
AlexxBot <alexx_96_sf17@hotmail.com>
2021-05-10 21:12:57 -0500

    set up the config

--- lib/config/agora.config.dart
+++ lib/config/agora.config.dart
@@ -2,10 +2,11 @@
 const appId = "cde3c6d037be4954ad72926be4aa5f21";
 
 /// Please refer to https://docs.agora.io/en/Agora%20Platform/token
-const token = "207eaddc2f8e40df972eab7ad34e8d48";
+const token =
+    "006cde3c6d037be4954ad72926be4aa5f21IADqAb+YP/UZNaL5bRmKdri418IwdvbDZGbOHHL1s5PFRxGBrEYAAAAAEADn5pQIPzGbYAEAAQA9MZtg";
 
 /// Your channel ID
-const channelId = "1";
+const channelId = "canal_test";
 
 /// Your int user ID
 const uid = 1;
